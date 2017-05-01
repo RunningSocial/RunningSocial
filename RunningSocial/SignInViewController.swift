80a300d343fe7b3e7e8fac038edf8c74b63b272b
Lane Faison <lane.faison@icloud.com>
2017-05-01 12:12:18 -0600

    Login added

--- RunningSocial/SignInViewController.swift
+++ RunningSocial/SignInViewController.swift
@@ -7,24 +7,40 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInViewController: UIViewController {
-
+    
     
     @IBOutlet weak var usernameTextField: UITextField!
-    
-    
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     @IBAction func loginTapped(_ sender: Any) {
         
+        FIRAuth.auth()?.signIn(withEmail: usernameTextField.text!, password: passwordTextField.text!, completion: { (user, error) in
+            print("We tried to sign in")
+            if error != nil {
+                print("We have error: \(error)")
+                FIRAuth.auth()?.createUser(withEmail: self.usernameTextField.text!, password: self.passwordTextField.text!, completion: { (user, error) in
+                    print("We tried to create a user")
+                    if error != nil {
+                        print("We tried to create a user")
+                    } else {
+                        print("Created user successfully")
+                        self.performSegue(withIdentifier: "signinsegue", sender: nil)
+                    }
+                })
+            } else {
+                print("Signed in successfully")
+                self.performSegue(withIdentifier: "signinsegue", sender: nil)
+            }
+        })
     }
-
-
+    
 }
 
